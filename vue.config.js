fe82b3d06e9d6531340fa1401475e31d39861a95
Gaurav Vaidya <gaurav@ggvaidya.com>
2021-08-05 16:06:39 -0400

    Added title to vue.config.js.

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      title: 'JSON Data Visualization and Transformation Tool',
+    },
+  }
+}
